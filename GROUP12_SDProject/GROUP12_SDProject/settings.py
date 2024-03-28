3f42ecb7de692e5f52d4189a5225e7a5c6d48d51
Eugene Tuhaise <genehaise@gmail.com>
2024-03-28 09:51:53 +0300

    Added static files

--- GROUP12_SDProject/GROUP12_SDProject/settings.py
+++ GROUP12_SDProject/GROUP12_SDProject/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfolder')
+
 
 STATICFILES_DIRS=[STATIC_DIR,]
 
